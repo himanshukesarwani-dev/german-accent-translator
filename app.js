3cb5244e887c94187af960050d4c4188cd519d2a
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-06 12:54:22 +0530

    chore: added ids and declared var

---
+++ app.js
@@ -0,0 +1,4 @@
+// variables 
+var inputText = document.querySelector("#text-input");
+var outputText = document.querySelector("#text-output");
+var btnTranslate = document.querySelector("#btn-translate");
