9ab4cbb5dc7669d1467ed963ea2b572b13386569
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-06 13:56:27 +0530

    added the final version

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var serverURL = "https://api.funtranslations.com/translate/german-accent.json";
 
+
+
+
 // URL Generator Function
 
 function makeURL(text) {
-    return serverURL + "?text=" + text;
+
+   return serverURL + "?text=" + text;
+    
 }
 
-//event handler function called when click happens
-
-btnTranslate.addEventListener("click", eventHandlerFunction)
 
 
 // error function
@@ -25,15 +27,22 @@ function errorHandler(error) {
     console.log("Troubles from Server: Try again later!", error);
     alert("Sorry! The server is causing some issues. Please try again later:")
 }
+
 // event handler function 
 
 function eventHandlerFunction()
 {
+   
     var text = inputText.value;
 
     fetch(makeURL(text))
-    .then(respone => Response.json())
+    .then(response => response.json())
     .then(json => outputText.innerText = json.contents.translated)
     .catch(errorHandler)
 
 }
+
+
+//event handler function called when click happens
+
+btnTranslate.addEventListener('click' , eventHandlerFunction)
