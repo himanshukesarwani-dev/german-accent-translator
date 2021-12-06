ead85b5285416a89075253471d3a23ea6ca4a4a3
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-06 13:13:18 +0530

    chore: added serverURL + makeURL function

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 var inputText = document.querySelector("#text-input");
 var outputText = document.querySelector("#text-output");
 var btnTranslate = document.querySelector("#btn-translate");
+
+
+// server URL
+
+var serverURL = "https://api.funtranslations.com/translate/german-accent.json";
+
+// URL Generator Function
+
+function makeURL(text) {
+    return serverURL + "?text=" + text;
+}
