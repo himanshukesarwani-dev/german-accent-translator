68d13fe6d934dec8a5eddcc3bf14dfdb374a785f
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-06 13:22:54 +0530

    chore :event handler and error Handler function

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ var serverURL = "https://api.funtranslations.com/translate/german-accent.json";
 function makeURL(text) {
     return serverURL + "?text=" + text;
 }
+
+//event handler function called when click happens
+
+btnTranslate.addEventListener("click", eventHandlerFunction)
+
+
+// error function
+
+function errorHandler(error) {
+    console.log("Troubles from Server: Try again later!", error);
+    alert("Sorry! The server is causing some issues. Please try again later:")
+}
+// event handler function 
+
+function eventHandlerFunction()
+{
+    var text = inputText.value;
+
+    fetch(makeURL(text))
+    .then(respone => Response.json())
+    .then(json => outputText.innerText = json.contents.translated)
+    .catch(errorHandler)
+
+}
